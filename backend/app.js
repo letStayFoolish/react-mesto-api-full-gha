fca1a2f23d91373cdfb5413f385fe81029e5d3ee
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-03 14:12:45 +0200

    Logger
    app.js:
    - app.use(cors({
        origin: ['http://localhost:3000', 'https://chili.nomoreparties.co', 'http://chili.nomoreparties.co'],
        credentials: true,
      }))

--- backend/app.js
+++ backend/app.js
@@ -3,27 +3,26 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookies = require('cookie-parser');
 const helmet = require('helmet');
-const { errors } = require('celebrate');
-const { PORT, DB_ADDRESS } = require('./config');
+const {errors} = require('celebrate');
+const {PORT, DB_ADDRESS} = require('./config');
 const routes = require('./routes/index');
 const limiter = require('./utils/limiter');
 const cors = require('cors')
-const { errorLogger, requestLogger } = require('./middlewares/logger')
+const {errorLogger, requestLogger} = require('./middlewares/logger')
 const errorsHandlers = require('./middlewares/errorsHandler');
 // old way: mongodb://localhost:27017/mestodb
 mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
 });
 const app = express();
-// app.use(cors({
-//   origin: ['http://localhost:3000', 'https://chili.nomoreparties.co', 'http://chili.nomoreparties.co'],
-//   credentials: true,
-// }))
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://chili.nomoreparties.co', 'http://chili.nomoreparties.co'],
+  credentials: true,
+}))
 app.use(helmet());
 app.use(cookies());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(limiter);
 // Collecting requests logger
 app.use(requestLogger)
