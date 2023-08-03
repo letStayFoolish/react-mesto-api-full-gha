156189d30d4d6254d946f4f16d3cb87d1b28532d
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-08-03 13:17:06 +0200

    Logger
    add middlewares/logger.js:
    - collecting requests logger
    - collecting errors logger
    
    app.js:
    - import errorLogger, requestLogger

--- backend/app.js
+++ backend/app.js
@@ -8,23 +8,30 @@ const { PORT, DB_ADDRESS } = require('./config');
 const routes = require('./routes/index');
 const limiter = require('./utils/limiter');
 const cors = require('cors')
+const { errorLogger, requestLogger } = require('./middlewares/logger')
 const errorsHandlers = require('./middlewares/errorsHandler');
 // old way: mongodb://localhost:27017/mestodb
 mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
 });
 const app = express();
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://chili.nomoreparties.co'],
-  credentials: true,
-}))
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://chili.nomoreparties.co', 'http://chili.nomoreparties.co'],
+//   credentials: true,
+// }))
+app.use(cors())
 app.use(helmet());
 app.use(cookies());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(limiter);
+// Collecting requests logger
+app.use(requestLogger)
 // Access to all routes:
 app.use(routes);
+// Collecting errors logger
+app.use(errorLogger)
+
 app.use(errors());
 app.use(errorsHandlers);
 app.listen(PORT, () => {
